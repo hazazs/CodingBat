2096e7ef5808bdf4d97d876615607a4a7e01b4c3
hazazs <hazafi.tibor@gmail.com>
2021-03-22 12:55:22 +0100

    CodingBat

--- Warmup-1/CodingBat.java
+++ Warmup-1/CodingBat.java
@@ -38,4 +38,52 @@ public class CodingBat {
         return n > 21 ? 2 * (n - 21) : 21 - n;
     }
     
+    /*
+    parrotTrouble
+    We have a loud talking parrot. The "hour" parameter is the current hour time in the range
+    0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return
+    true if we are in trouble.
+    */
+    public boolean parrotTrouble(boolean talking, int hour) {
+        return talking && (hour < 7 || hour > 20);
+    }
+    
+    /*
+    makes10
+    Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+    */
+    public boolean makes10(int a, int b) {
+        return a == 10 || b == 10 || a + b == 10;
+    }
+    
+    /*
+    nearHundred
+    Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes
+    the absolute value of a number.
+    */
+    public boolean nearHundred(int n) {
+        return Math.abs(100 - n) <= 10 || Math.abs(200 - n) <= 10;
+    }
+    
+    /*
+    posNeg
+    Given 2 int values, return true if one is negative and one is positive. Except if the
+    parameter "negative" is true, then return true only if both are negative.
+    */
+    public boolean posNeg(int a, int b, boolean negative) {
+    if (negative)
+        return a < 0 && b < 0;
+    return a < 0 && b > 0 || a > 0 && b < 0;
+    }
+    
+    /*
+    notString
+    Given a string, return a new string where "not " has been added to the front. However, if
+    the string already begins with "not", return the string unchanged. Note: use .equals() to
+    compare 2 strings.
+    */
+    public String notString(String str) {
+        return str.startsWith("not") ? str : "not " + str;
+    }
+    
 }
