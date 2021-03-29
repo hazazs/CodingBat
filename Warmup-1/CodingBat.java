ffe7afd777c3e8f5e38b68296cc632e4ba0f4c03
hazazs <hazafi.tibor@gmail.com>
2021-03-29 15:44:45 +0200

    CodingBat

--- Warmup-1/CodingBat.java
+++ Warmup-1/CodingBat.java
@@ -136,4 +136,49 @@ public class CodingBat {
         return n % 3 == 0 || n % 5 == 0;
     }
     
+    /*
+    front22
+    Given a string, take the first 2 chars and return the string with the 2 chars added at both
+    the front and back, so "kitten" yields"kikittenki". If the string length is less than 2, use
+    whatever chars are there.
+     */
+    public String front22(String str) {
+        if (str.length() < 2)
+            return str + str + str;
+        return str.substring(0, 2) + str + str.substring(0, 2);
+    }
+    
+    /*
+    startHi
+    Given a string, return true if the string starts with "hi" and false otherwise.
+     */
+    public boolean startHi(String str) {
+        return str.length() >= 2 && str.substring(0, 2).equals("hi");
+    }
+    
+    /*
+    icyHot
+    Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+     */
+    public boolean icyHot(int temp1, int temp2) {
+        return (temp1 < 0 && temp2 > 100) || (temp2 < 0 && temp1 > 100);
+    }
+    
+    /*
+    in1020
+    Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+     */
+    public boolean in1020(int a, int b) {
+        return (a >= 10 && a <= 20) || (b >= 10 && b <= 20);
+    }
+    
+    /*
+    hasTeen
+    We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 3 int values,
+    return true if 1 or more of them are teen.
+     */
+    public boolean hasTeen(int a, int b, int c) {
+        return (a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19);
+    }
+    
 }
