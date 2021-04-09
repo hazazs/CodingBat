e9e8972bf8ff786b062a8bf312ba4d590315f5b0
hazazs <hazafi.tibor@gmail.com>
2021-04-09 15:42:28 +0200

    CodingBat

---
+++ 03. String-1/CodingBat.java
@@ -0,0 +1,11 @@
+public class CodingBat {
+    
+    /*
+    helloName
+    Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+     */
+    public String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+    
+}
