b45ebd8b85bcfcbea8659ffb54b54b5e19eda7ff
hazazs <hazafi.tibor@gmail.com>
2021-04-09 15:39:08 +0200

    CodingBat

--- Warmup-2/CodingBat.java
+++ Warmup-2/CodingBat.java
@@ -37,4 +37,178 @@ public class CodingBat {
         return count;
     }
     
+    /*
+    doubleX
+    Given a string, return true if the first instance of "x" in the string is immediately followed
+    by another "x".
+     */
+    boolean doubleX(String str) {
+        int index = str.indexOf("x");
+        return index >= 0 && index != str.length()-1 && str.charAt(index+1) == 'x';
+    }
+    
+    /*
+    stringBits
+    Given a string, return a new string made of every other char starting with the first, so
+    "Hello" yields "Hlo".
+     */
+    public String stringBits(String str) {
+        String ret = "";
+        for (int i = 0; i < str.length(); i += 2)
+            ret += str.charAt(i);
+        return ret;
+    }
+    
+    /*
+    stringSplosion
+    Given a non-empty string like "Code" return a string like "CCoCodCode".
+     */
+    public String stringSplosion(String str) {
+        String ret = "";
+        for (int i = 1; i < str.length() + 1; i++)
+            ret += str.substring(0, i);
+        return ret;
+    }
+    
+    /*
+    last2
+    Given a string, return the count of the number of times that a substring length 2 appears
+    in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't
+    count the end substring).
+     */
+    public int last2(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length()-2; i++)
+            if (str.substring(i, i+2).equals(str.substring(str.length()-2)))
+                count++;
+        return count;
+    }
+    
+    /*
+    arrayCount9
+    Given an array of ints, return the number of 9's in the array.
+     */
+    public int arrayCount9(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++)
+            if (nums[i] == 9)
+                count++;
+        return count;
+    }
+    
+    /*
+    arrayFront9
+    Given an array of ints, return true if one of the first 4 elements in the array is a 9. The
+    array length may be less than 4.
+     */
+    public boolean arrayFront9(int[] nums) {
+        for (int i = 0; i < (nums.length > 4 ? 4 : nums.length); i++)
+            if (nums[i] == 9)
+                return true;
+        return false;
+    }
+    
+    /*
+    array123
+    Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array
+    somewhere.
+     */
+    public boolean array123(int[] nums) {
+        for (int i = 0; i < nums.length-2; i++)
+            if (nums[i] == 1 && nums[i+1] == 2 && nums[i+2] == 3)
+                return true;
+        return false;
+    }
+    
+    /*
+    stringMatch
+    Given 2 strings, a and b, return the number of the positions where they contain the same
+    length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az"
+    substrings appear in the same place in both strings.
+     */
+    public int stringMatch(String a, String b) {
+        int count = 0;
+        for (int i = 0; i < (a.length() >= b.length() ? b.length() : a.length())-1; i++)
+            if (a.substring(i, i+2).equals(b.substring(i, i+2)))
+                count++;
+        return count;
+    }
+    
+    /*
+    stringX
+    Given a string, return a version where all the "x" have been removed. Except an "x" at the
+    very start or end should not be removed.
+     */
+    public String stringX(String str) {
+        String ret = "";
+        for (int i = 0; i < str.length(); i++)
+            if (i == 0 || str.charAt(i) != 'x' || i == str.length()-1)
+                ret += str.charAt(i);
+        return ret;
+    }
+    
+    /*
+    altPairs
+    Given a string, return a string made of the chars at indexes 0,1, 4,5, 8,9 ... so "kittens"
+    yields "kien".
+     */
+    public String altPairs(String str) {
+        String ret = "";
+        for (int i = 0; i < str.length(); i += 4)
+            ret += i < str.length()-1 ? str.substring(i, i+2) : str.charAt(i);
+        return ret;
+    }
+    
+    /*
+    stringYak
+    Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak"
+    are removed, but the "a" can be any char. The "yak" strings will not overlap.
+     */
+    public String stringYak(String str) {
+        String ret = "";
+        for (int i = 0; i < str.length(); i++)
+            if (i < str.length()-2 && str.charAt(i) == 'y' && str.charAt(i+2) == 'k')
+                i += 2;
+            else ret += str.charAt(i);
+        return ret;
+    }
+    
+    /*
+    array667
+    Given an array of ints, return the number of times that two 6's are next to each other in
+    the array. Also count instances where the second "6" is actually a 7.
+     */
+    public int array667(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length-1; i++)
+            if (nums[i] == 6 && (nums[i+1] == 6 || nums[i+1] == 7))
+                count++;
+        return count;
+    }
+    
+    /*
+    noTriples
+    Given an array of ints, we'll say that a triple is a value appearing 3 times in a row in the
+    array. Return true if the array does not contain any triples.
+     */
+    public boolean noTriples(int[] nums) {
+        for (int i = 0; i < nums.length-2; i++)
+            if (nums[i] == nums[i+1] && nums[i] == nums[i+2])
+                return false;
+        return true;
+    }
+    
+    /*
+    has271
+    Given an array of ints, return true if it contains a 2, 7, 1 pattern: a value, followed by the
+    value plus 5, followed by the value minus 1. Additionally the 271 counts even if the "1"
+    differs by 2 or less from the correct value.
+     */
+    public boolean has271(int[] nums) {
+        for (int i = 0; i < nums.length-2; i++)
+            if (nums[i+1] == nums[i]+5 && Math.abs(nums[i+2] - nums[i]+1) <= 2)
+                return true;
+        return false;
+    }
+    
 }
