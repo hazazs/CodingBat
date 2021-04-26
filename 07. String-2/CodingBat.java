8bd786e1b5c91ee54bffa60e06913682c357b0ab
hazazs <hazafi.tibor@gmail.com>
2021-04-26 15:51:08 +0200

    CodingBat

--- 07. String-2/CodingBat.java
+++ 07. String-2/CodingBat.java
@@ -6,8 +6,9 @@ public class CodingBat {
      */
     public String doubleChar(String str) {
         StringBuilder ret = new StringBuilder(2 * str.length());
-        for (int i = 0; i < str.length(); i++)
+        for (int i = 0; i < str.length(); i++) {
             ret.append(str.substring(i, i + 1)).append(str.substring(i, i + 1));
+        }
         return ret.toString();
     }
 
@@ -17,11 +18,12 @@ public class CodingBat {
      */
     public int countHi(String str) {
         int count = 0;
-        for (int i = 0; i < str.length() - 1; i++)
+        for (int i = 0; i < str.length() - 1; i++) {
             count = str.substring(i, i + 2).equals("hi") ? count + 1 : count;
+        }
         return count;
     }
-    
+
     /*
     catDog
     Return true if the string "cat" and "dog" appear the same number of times in the given
@@ -36,7 +38,7 @@ public class CodingBat {
         }
         return catNo == dogNo;
     }
-    
+
     /*
     countCode
     Return the number of times that the string "code" appears anywhere in the given string,
@@ -44,12 +46,14 @@ public class CodingBat {
      */
     public int countCode(String str) {
         int count = 0;
-        for (int i = 0; i < str.length() - 3; i++)
-            if (str.substring(i, i + 2).equals("co") && str.charAt(i + 3) == 'e')
+        for (int i = 0; i < str.length() - 3; i++) {
+            if (str.substring(i, i + 2).equals("co") && str.charAt(i + 3) == 'e') {
                 count++;
+            }
+        }
         return count;
     }
-    
+
     /*
     endOther
     Given two strings, return true if either of the strings appears at the very end of the other
@@ -60,13 +64,14 @@ public class CodingBat {
         String aLow = a.toLowerCase();
         String bLow = b.toLowerCase();
         if (a.length() > b.length() || a.length() < b.length()) {
-            if (a.length() > b.length())
+            if (a.length() > b.length()) {
                 return a.substring(a.length() - b.length()).toLowerCase().equals(bLow);
+            }
             return b.substring(b.length() - a.length()).toLowerCase().equals(aLow);
         }
         return a.toLowerCase().equals(b.toLowerCase());
     }
-    
+
     /*
     xyzThere
     Return true if the given string contains an appearance of "xyz" where the xyz is not
@@ -82,7 +87,7 @@ public class CodingBat {
         }
         return str.startsWith("xyz");
     }
-    
+
     /*
     bobThere
     Return true if the given string contains a "bob" string, but where the middle 'o' char can be
@@ -98,7 +103,7 @@ public class CodingBat {
         }
         return false;
     }
-    
+
     /*
     xyBalance
     We'll say that a String is xy-balanced if for all the 'x' chars in the string, there exists a 'y'
@@ -106,12 +111,14 @@ public class CodingBat {
     balance multiple 'x's. Return true if the given string is xy-balanced.
      */
     public boolean xyBalance(String str) {
-        for (int i = str.lastIndexOf("y") + 1; i < str.length(); i++)
-            if (str.charAt(i) == 'x')
+        for (int i = str.lastIndexOf('y') + 1; i < str.length(); i++) {
+            if (str.charAt(i) == 'x') {
                 return false;
+            }
+        }
         return true;
     }
-    
+
     /*
     mixString
     Given two strings, a and b, create a bigger string made of the first char of a, the first char
@@ -120,8 +127,9 @@ public class CodingBat {
      */
     public String mixString(String a, String b) {
         StringBuilder ret = new StringBuilder(a.length() + b.length());
-        for (int i = 0; i < Math.min(a.length(), b.length()); i++)
+        for (int i = 0; i < Math.min(a.length(), b.length()); i++) {
             ret.append(a.charAt(i)).append(b.charAt(i));
+        }
         if (a.length() == b.length()) {
             return ret.toString();
         }
@@ -130,7 +138,7 @@ public class CodingBat {
         }
         return ret.append(b.substring(a.length())).toString();
     }
-    
+
     /*
     repeatEnd
     Given a string and an int n, return a string made of n repetitions of the last n characters of
@@ -138,11 +146,12 @@ public class CodingBat {
      */
     public String repeatEnd(String str, int n) {
         StringBuilder ret = new StringBuilder(n * n);
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             ret.append(str.substring(str.length() - n));
+        }
         return ret.toString();
     }
-    
+
     /*
     repeatFront
     Given a string and an int n, return a string made of the first n characters of the string,
@@ -154,11 +163,12 @@ public class CodingBat {
         for (int i = n; i > 0; i--, len += i) {
         }
         StringBuilder ret = new StringBuilder(len);
-        for (int i = n; i > 0; i--)
+        for (int i = n; i > 0; i--) {
             ret.append(str.substring(0, i));
+        }
         return ret.toString();
     }
-    
+
     /*
     repeatSeparator
     Given two strings, word and a separator sep, return a big string made of count
@@ -167,11 +177,12 @@ public class CodingBat {
     public String repeatSeparator(String word, String sep, int count) {
         int len = count * word.length() + (count - 1) * sep.length();
         StringBuilder ret = new StringBuilder(len);
-        for (int i = 0; i < count; i++)
+        for (int i = 0; i < count; i++) {
             ret.append(i == count - 1 ? word : word + sep);
+        }
         return ret.toString();
     }
-    
+
     /*
     prefixAgain
     Given a string, consider the prefix string made of the first N chars of the string. Does that
@@ -181,7 +192,7 @@ public class CodingBat {
     public boolean prefixAgain(String str, int n) {
         return str.substring(1).contains(str.substring(0, n));
     }
-    
+
     /*
     xyzMiddle
     Given a string, does "xyz" appear in the middle of the string? To define middle, we'll say
@@ -201,7 +212,7 @@ public class CodingBat {
         }
         return false;
     }
-    
+
     /*
     getSandwich
     A sandwich is two pieces of bread with something in between. Return the string that is
@@ -209,23 +220,26 @@ public class CodingBat {
     string "" if there are not two pieces of bread.
      */
     public String getSandwich(String str) {
-        if (str.indexOf("bread") != str.lastIndexOf("bread"))
+        if (str.indexOf("bread") != str.lastIndexOf("bread")) {
             return str.substring(str.indexOf("bread") + 5, str.lastIndexOf("bread"));
+        }
         return "";
     }
-    
+
     /*
     sameStarChar
     Returns true if for every '*' (star) in the string, if there are chars both immediately before
     and after the star, they are the same.
      */
     public boolean sameStarChar(String str) {
-        for (int i = 1; i < str.length() - 1; i++)
-            if (str.charAt(i) == '*' && str.charAt(i - 1) != str.charAt(i + 1))
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (str.charAt(i) == '*' && str.charAt(i - 1) != str.charAt(i + 1)) {
                 return false;
+            }
+        }
         return true;
     }
-    
+
     /*
     oneTwo
     Given a string, compute a new string by moving the first char to come after the next two
@@ -233,12 +247,13 @@ public class CodingBat {
     "abcdef" yields "bcaefd". Ignore any group of fewer than 3 chars at the end.
      */
     public String oneTwo(String str) {
-        StringBuilder ret = new StringBuilder(str.length());
-        for (int i = 0; i < str.length() - (str.length() % 3); i += 3)
+        StringBuilder ret = new StringBuilder(str.length() < 3 ? 0 : str.length());
+        for (int i = 0; i < str.length() - (str.length() % 3); i += 3) {
             ret.append(str.charAt(i + 1)).append(str.charAt(i + 2)).append(str.charAt(i));
+        }
         return ret.toString();
     }
-    
+
     /*
     zipZap
     Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending
@@ -250,15 +265,17 @@ public class CodingBat {
         if (str.length() > 0) {
             ret.append(str.charAt(0));
         }
-        for (int i = 1; i < str.length() - 1; i++)
-            if (!(str.charAt(i - 1) == 'z' && str.charAt(i + 1) == 'p'))
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (!(str.charAt(i - 1) == 'z' && str.charAt(i + 1) == 'p')) {
                 ret.append(str.charAt(i));
+            }
+        }
         if (str.length() > 1) {
             return ret.append(str.substring(str.length() - 1)).toString();
         }
         return ret.toString();
     }
-    
+
     /*
     starOut
     Return a version of the given string, where for every star (*) in the string the star and the
@@ -271,12 +288,14 @@ public class CodingBat {
             numberOfStars += str.charAt(i) == '*' ? 1 : 0;
         }
         StringBuilder ret = new StringBuilder(str.length() - numberOfStars);
-        for (int i = 0; i < str.length(); i++)
-            if ((i == 0 || str.charAt(i - 1) != '*') && str.charAt(i) != '*' && (i == str.length() - 1 || str.charAt(i + 1) != '*'))
+        for (int i = 0; i < str.length(); i++) {
+            if ((i == 0 || str.charAt(i - 1) != '*') && str.charAt(i) != '*' && (i == str.length() - 1 || str.charAt(i + 1) != '*')) {
                 ret.append(str.charAt(i));
+            }
+        }
         return ret.toString();
     }
-    
+
     /*
     plusOut
     Given a string and a non-empty word string, return a version of the original String where
@@ -285,14 +304,17 @@ public class CodingBat {
      */
     public String plusOut(String str, String word) {
         StringBuilder ret = new StringBuilder(str.length());
-          for (int i = 0; i < str.length(); i++)
-                if (i < str.length() - word.length() + 1 && str.substring(i, i + word.length()).equals(word)) {
-                    ret.append(word);
-                    i += word.length() - 1;
-                } else ret.append("+");
+        for (int i = 0; i < str.length(); i++) {
+            if (i < str.length() - word.length() + 1 && str.substring(i, i + word.length()).equals(word)) {
+                ret.append(word);
+                i += word.length() - 1;
+            } else {
+                ret.append("+");
+            }
+        }
         return ret.toString();
     }
-    
+
     /*
     wordEnds
     Given a string and a non-empty word string, return a string made of each char just before
@@ -301,15 +323,18 @@ public class CodingBat {
      */
     public String wordEnds(String str, String word) {
         StringBuilder ret = new StringBuilder(0);
-        for (int i = 0; i < str.length() - word.length() + 1; i++)
+        for (int i = 0; i < str.length() - word.length() + 1; i++) {
             if (str.substring(i, i + word.length()).equals(word)) {
-                if (i > 0)
+                if (i > 0) {
                     ret.append(str.charAt(i - 1));
-                if (i < str.length() - word.length())
+                }
+                if (i < str.length() - word.length()) {
                     ret.append(str.charAt(i + word.length()));
+                }
                 i += word.length() - 1;
             }
+        }
         return ret.toString();
     }
-    
+
 }
